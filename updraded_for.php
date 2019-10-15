9b9de7aa22e8cf063c7f42d6f5b80d1b032006c7
DanielROC-dev <dkempers@roc-dev.com>
2019-10-15 08:45:41 +0200

    Uitwerking van de exercise

---
+++ updraded_for.php
@@ -0,0 +1,20 @@
+<?php
+
+$meme = array(
+"Doge",
+"Walter",
+"baby",
+"peter_belcher_from_american_dad"
+);
+$numbers = array(
+1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
+);
+foreach ($meme as $value) {
+	echo "$value" . PHP_EOL;
+}
+foreach ($numbers as $count20) {
+	echo "$count20" . PHP_EOL;
+}
+exit();
+?>
+
